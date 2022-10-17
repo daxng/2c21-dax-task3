bb00cfe02cada98ad51f3202e66ea64c4f52f62e
daxng <115450768+daxng@users.noreply.github.com>
2022-10-17 14:36:45 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,54 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(randint(1, 6))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(9)
-    basic.showNumber(8)
-    basic.showNumber(7)
-    basic.showNumber(6)
-    basic.showNumber(5)
-    basic.showNumber(4)
-    basic.showNumber(3)
-    basic.showNumber(2)
-    basic.showNumber(1)
-    basic.showNumber(0)
-})
-basic.forever(function () {
-	
+    count = 9
+    basic.showNumber(count)
+    for (let index = 0; index < 9; index++) {
+        count += -1
+        basic.showNumber(count)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
