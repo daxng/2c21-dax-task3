7fbbbe6b74374aebf37359d17d20ddaf9d065f87
daxng <115450768+daxng@users.noreply.github.com>
2022-10-17 14:15:23 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(9)
+    basic.showNumber(8)
+    basic.showNumber(7)
+    basic.showNumber(6)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showNumber(0)
+})
 basic.forever(function () {
 	
 })
