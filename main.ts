1bd8cf844bd9b3d9cfdc6d2993eb1b40ed1de61d
daxng <115450768+daxng@users.noreply.github.com>
2022-10-17 14:49:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,13 @@ let count = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(randint(1, 6))
 })
+input.onButtonPressed(Button.AB, function () {
+    count = 0
+})
 input.onButtonPressed(Button.B, function () {
     count = 9
     basic.showNumber(count)
-    for (let index = 0; index < 9; index++) {
+    while (count > 0) {
         count += -1
         basic.showNumber(count)
     }
